d5b681ac59463efb9398842b3686eb0704069f19
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-06 23:55:04 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :category, presence: true
-  validates :content, presence: true
+  validates :content, length: {minimum: 9}
 end
